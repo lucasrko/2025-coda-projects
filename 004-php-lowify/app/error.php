cdf33df939bf911cd82da94964f13fa76a616812
Lucas <lucas.milliere@coda-student.school>
2025-12-02 13:36:00 +0100

    Lowify mise à jour error

--- 004-php-lowify/app/error.php
+++ 004-php-lowify/app/error.php
@@ -9,12 +9,22 @@ $errorMessage = htmlspecialchars($errorMessage, ENT_QUOTES, 'UTF-8');
 
 // Génération du contenu HTML
 $html = <<<HTML
+<head>
+<style>
+body{
+background-color: #2c2c2c;
+text-align: center;
+}
+</style>
+</head>
     <div style="padding:20px;">
         <h1 style="color:red;">Erreur</h1>
         <p>$errorMessage</p>
+        <button>
         <a href="index.php" style="color:#1DB954; font-weight:bold;">
             ➜ Retour à l'accueil
         </a>
+        </button>
     </div>
 HTML;
 
